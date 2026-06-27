d892cfc9895b87036f6e2ef08cbadd7d2e52b677
Emmett Rayes <emmett.rayes@icloud.com>
2026-06-27 17:34:07 +0200

    implemented NaiveRecursionInterpreter

---
+++ src/main/scala/parser/NaiveRecursionInterpreter.scala
@@ -0,0 +1,15 @@
+package neocheck
+package parser
+
+/** A partial implementation of an interpreter of [[ParserAlgebra]] for [[Parser]].
+ *
+ * Uses a naive fixpoint combinator which can lead to stack overflow on left-recursive parsers.
+ *
+ * @tparam Input the type of the input to be parsed.
+ */
+trait NaiveRecursionInterpreter[Input] extends ParserAlgebra[[Output] =>> Parser[Input, Output]] {
+  override def recursive[Output](p: (=> Parser[Input, Output]) => Parser[Input, Output]): Parser[Input, Output] = {
+    lazy val rec: Parser[Input, Output] = input => p(rec).parse(input)
+    rec
+  }
+}
