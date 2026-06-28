d153d2717f19078b7b90ca49e2da8cff33a65efc
Emmett Rayes <emmett.rayes@icloud.com>
2026-06-28 10:37:39 +0200

    implemented lifted recursive operation in NaiveRecursionInterpreter

--- src/main/scala/parser/NaiveRecursionInterpreter.scala
+++ src/main/scala/parser/NaiveRecursionInterpreter.scala
@@ -1,6 +1,8 @@
 package neocheck
 package parser
 
+import scala.Tuple.Size
+
 /** A partial implementation of an interpreter of [[ParserAlgebra]] for [[Parser]].
  *
  * Uses a naive fixpoint combinator which can lead to stack overflow on left-recursive parsers.
@@ -10,6 +12,19 @@ package parser
 trait NaiveRecursionInterpreter[Input] extends ParserAlgebra[ParserF[Input]] {
   override def recursive[Output](p: Parser[Input, Output] => Parser[Input, Output]): Parser[Input, Output] = {
     lazy val rec: Parser[Input, Output] = input => p(rec).parse(input)
+    rec
+  }
+
+  override def recursive[Outputs <: Tuple](p: Tuple.Map[Outputs, ParserF[Input]] => Tuple.Map[Outputs, ParserF[Input]])
+                                          (using size: ValueOf[Size[Outputs]]): Tuple.Map[Outputs, ParserF[Input]] = {
+    lazy val rec: Tuple.Map[Outputs, ParserF[Input]] = {
+      val parsers = Array.tabulate[Parser[Input, ?]](size.value) {
+        i => {
+          input => p(rec).productElement(i).asInstanceOf[Parser[Input, ?]].parse(input)
+        }
+      }
+      Tuple.fromArray(parsers).asInstanceOf[Tuple.Map[Outputs, ParserF[Input]]]
+    }
     rec
   }
 }
